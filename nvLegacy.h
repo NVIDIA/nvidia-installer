eb93cf37a31b395bb7d8cb69ebf974ed54ea0322
Aaron Plattner <aplattner@nvidia.com>
2008-06-17 23:27:19 -0700

    177.13

--- nvLegacy.h
+++ nvLegacy.h
@@ -26,6 +26,7 @@ typedef struct _LEGACY_STRINGS {
  * This table describes how we should refer to legacy branches.
  */
 static const LEGACY_STRINGS LegacyStrings[] = {
+    { 3, "173.14.xx" },
     { 2, "96.43.xx" },
     { 1, "71.86.xx" }
 };
@@ -39,6 +40,13 @@ static const LEGACY_INFO LegacyList[] = {
     { 0x002C,   1,      "Vanta/Vanta LT" },
     { 0x002D,   1,      "RIVA TNT2 Model 64/Model 64 Pro" },
     { 0x00A0,   1,      "Aladdin TNT2" },
+    { 0x00FA,   3,      "GeForce PCX 5750" },
+    { 0x00FB,   3,      "GeForce PCX 5900" },
+    { 0x00FC,   3,      "Quadro FX 330" },
+    { 0x00FC,   3,      "GeForce PCX 5300" },
+    { 0x00FD,   3,      "Quadro FX 330" },
+    { 0x00FD,   3,      "Quadro NVS 280 PCI-E" },
+    { 0x00FE,   3,      "Quadro FX 1300" },
     { 0x0100,   1,      "GeForce 256" },
     { 0x0101,   1,      "GeForce DDR" },
     { 0x0103,   1,      "Quadro" },
@@ -89,7 +97,45 @@ static const LEGACY_INFO LegacyList[] = {
     { 0x0286,   2,      "GeForce4 4200 Go" },
     { 0x0288,   2,      "Quadro4 980 XGL" },
     { 0x0289,   2,      "Quadro4 780 XGL" },
-    { 0x028C,   2,      "Quadro4 700 GoGL" }
+    { 0x028C,   2,      "Quadro4 700 GoGL" },
+    { 0x0301,   3,      "GeForce FX 5800 Ultra" },
+    { 0x0302,   3,      "GeForce FX 5800" },
+    { 0x0308,   3,      "Quadro FX 2000" },
+    { 0x0309,   3,      "Quadro FX 1000" },
+    { 0x0311,   3,      "GeForce FX 5600 Ultra" },
+    { 0x0312,   3,      "GeForce FX 5600" },
+    { 0x0314,   3,      "GeForce FX 5600XT" },
+    { 0x031A,   3,      "GeForce FX Go5600" },
+    { 0x031B,   3,      "GeForce FX Go5650" },
+    { 0x031C,   3,      "Quadro FX Go700" },
+    { 0x0320,   3,      "GeForce FX 5200" },
+    { 0x0321,   3,      "GeForce FX 5200 Ultra" },
+    { 0x0322,   3,      "GeForce FX 5200" },
+    { 0x0323,   3,      "GeForce FX 5200LE" },
+    { 0x0324,   3,      "GeForce FX Go5200" },
+    { 0x0325,   3,      "GeForce FX Go5250" },
+    { 0x0326,   3,      "GeForce FX 5500" },
+    { 0x0327,   3,      "GeForce FX 5100" },
+    { 0x0328,   3,      "GeForce FX Go5200 32M/64M" },
+    { 0x032A,   3,      "Quadro NVS 55/280 PCI" },
+    { 0x032B,   3,      "Quadro FX 500/FX 600" },
+    { 0x032C,   3,      "GeForce FX Go53xx" },
+    { 0x032D,   3,      "GeForce FX Go5100" },
+    { 0x0330,   3,      "GeForce FX 5900 Ultra" },
+    { 0x0331,   3,      "GeForce FX 5900" },
+    { 0x0332,   3,      "GeForce FX 5900XT" },
+    { 0x0333,   3,      "GeForce FX 5950 Ultra" },
+    { 0x0334,   3,      "GeForce FX 5900ZT" },
+    { 0x0338,   3,      "Quadro FX 3000" },
+    { 0x033F,   3,      "Quadro FX 700" },
+    { 0x0341,   3,      "GeForce FX 5700 Ultra" },
+    { 0x0342,   3,      "GeForce FX 5700" },
+    { 0x0343,   3,      "GeForce FX 5700LE" },
+    { 0x0344,   3,      "GeForce FX 5700VE" },
+    { 0x0347,   3,      "GeForce FX Go5700" },
+    { 0x0348,   3,      "GeForce FX Go5700" },
+    { 0x034C,   3,      "Quadro FX Go1000" },
+    { 0x034E,   3,      "Quadro FX 1100" }
 };
 
 #endif /* __NV_LEGACY_H */
